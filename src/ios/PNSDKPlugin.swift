29d22d0c2753bfaa4ac81a86f72d64bf1f35655b
yezenalnafei <yezen.alnafei@talktalkplc.com>
2018-04-14 19:02:51 +0100

    added push notification extension

--- src/ios/PNSDKPlugin.swift
+++ src/ios/PNSDKPlugin.swift
@@ -1,9 +1,24 @@
 import Foundation
+import UserNotifications
 
 @objc(LPMessagingSDKPlugin) class LPMessagingSDKPlugin: CDVPlugin {
-
-
-
+    
+    static var pnToken: String?
+    
+    override func pluginInitialize() {
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+                (granted, error) in
+                print("Permission granted: \(granted)")
+            }
+        } else {
+            let settings : UIUserNotificationSettings = UIUserNotificationSettings(types:UIUserNotificationType(rawValue: UIUserNotificationType.RawValue(UInt8(UIUserNotificationType.alert.rawValue)|UInt8(UIUserNotificationType.sound.rawValue))), categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+    }
+    
     // Prepare a callback to trigger a response to the JS consumer when native commands have finished
     func prepareCallback(_ from: CDVInvokedUrlCommand) -> LPCordovaCallback {
         return LPCordovaCallback(delegate: commandDelegate, command: from)
@@ -23,17 +38,28 @@ import Foundation
 
     @objc func get_push_notification_token(_ command: CDVInvokedUrlCommand) {
 
-         if let pushToken = appDelegatePushNotificationToken() {
+        if (LPMessagingSDKPlugin.pnToken != nil) {
             
-            let resultNSString = pushToken.map { String(format: "%02.2hhx", $0) }.joined()
-                        
             prepareCallback(command)
-            .ok(resultNSString, keepCallback: true)
+            .ok(LPMessagingSDKPlugin.pnToken, keepCallback: true)
 
         } else{
             prepareCallback(command)
             .ok(0, keepCallback: true)
+        }
     }
 }
 
+extension AppDelegate{
+    open override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        var token = ""
+        for i in 0..<deviceToken.count {
+            token = token + String(format: "%02.2hhx", arguments: [deviceToken[i]])
+        }
+        LPMessagingSDKPlugin.pnToken = token
+    }
+    
+   open override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
 }
