6b3c150ff48404390cfa5c0c2b718b47d3ed5ee4
yezenalnafei <yezen.alnafei@talktalkplc.com>
2018-04-19 20:27:44 +0100

    adding tings

--- www/PushNotificationToken.ts
+++ www/PushNotificationToken.ts
@@ -2,7 +2,7 @@
 import { exec } from 'cordova';
 
 /** The LivePerson Messaging SDK service name (targeted by Cordova). */
-const lpSdkService = 'LPMessagingSDK';
+const lpSdkService = 'PNMessagingSDK';
 
 //#region Type
 
